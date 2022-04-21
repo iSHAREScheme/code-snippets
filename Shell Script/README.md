b17b8b5efce9fc53b89a59ce76a9ea249f84ab7b
Rajiv Rajani (iSHARE) <rajiv@ishare.eu>
2022-04-21 12:58:37 +0200

    Initial commit

---
+++ Shell Script/README.md
@@ -0,0 +1,16 @@
+Just dowload the script file and make sure it is executable
+
+Usage: ./ckxp12.sh <p12file>
+
+Note: it prompts for p12 file password which you should have in order to extract these files.
+
+Shell script which accepts p12 file and extracts following files        
+<p12filename>pub.pem - having just the public cert                      
+<p12filename>pubfc.pem - full chain of public cert                      
+<p12filename>priv.key - unencrypted private RSA key                     
+                                                                    
+Note: the above files are created in current directory where the script is executed                
+                                                                    
+Version 1.0                                                             
+Developed by: Rajiv Rajani (rajiv@ishare.eu)                            
+Disclaimer: Please handle private keys with care and securely, author is not responsible in any way for any damage caused.
---
+++ Shell Script/ckxp12.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+#############################################################################
+#   Shell script which accepts p12 file and extracts following files        #
+#   <p12filename>pub.pem - having just the public cert                      #
+#   <p12filename>pubfc.pem - full chain of public cert                      #
+#   <p12filename>priv.key - unencrypted private RSA key                     #
+#                                                                           #
+#   Note: the above files are created in current directory                  #
+#                                                                           #
+#   Version 1.0                                                             #
+#   Developed by: Rajiv Rajani (rajiv@ishare.eu)                            #
+#   Disclaimer: Please handle private keys with care and securely,          #
+#   author is not responsible in any way for any damage caused              #
+#                                                                           #
+#############################################################################
+
+#capture arg
+PKCSFL=$1
+
+#check if arg provided
+if [[ $PKCSFL == "" ]]; then
+    printf "Usage: ckxp12 <p12 file>\n\n"
+    exit 1
+fi
+
+#check if file exists
+if [ ! -f $PKCSFL ]; then
+    printf "File $PKCSFL does not exist. Please check and try again.\n"
+fi
+
+#extract file base name
+filename="${PKCSFL##*/}"
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+#check if openssl is installed
+if ! command -v openssl &> /dev/null
+then
+    printf "openssl not installed. please install it first.\n"
+    exit
+fi
+
+#get p12 file password
+printf "Enter p12 password: "
+read -s p12pass
+
+printf "\n"
+
+p12pass="pass:"$p12pass
+#extract pem (public key certificate) and RSA private key
+pubfilename=$filename"pub.pem"
+fcfilename=$filename"pubfc.pem"
+privfilename=$filename"priv.key"
+
+openssl pkcs12 -in $PKCSFL -clcerts -nokeys -passin $p12pass -out $pubfilename
+if [ -f $pubfilename ]; then
+    printf "$pubfilename successfully created.\n"
+fi
+
+openssl pkcs12 -in $PKCSFL -nokeys -passin $p12pass -out $fcfilename
+if [ -f $fcfilename ]; then
+    printf "$fcfilename successfully created.\n"
+fi
+
+openssl pkcs12 -in $PKCSFL -nocerts -nodes -passin $p12pass | openssl rsa > $privfilename
+if [ -f $privfilename ]; then
+    printf "$privfilename successfully created.\n"
+fi
+
+printf "Files can be found in current directory.\n"
+
+#end
