2da917c23f627236ca681c2b390b115363c0d6b9
Rajiv Rajani (iSHARE) <rajiv@ishare.eu>
2022-05-25 17:30:01 +0200

    updated comments

--- Cert_Key_Extractor/ckxp12.sh
+++ Cert_Key_Extractor/ckxp12.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
-#############################################################################
-#   Shell script which accepts p12 file and extracts following files        #
-#   <p12filename>pub.pem - having just the public cert                      #
-#   <p12filename>pubfc.pem - full chain of public cert                      #
-#   <p12filename>priv.key - unencrypted private RSA key                     #
-#                                                                           #
-#   Note: the above files are created in current directory                  #
-#                                                                           #
-#   Version 1.0                                                             #
-#   Developed by: Rajiv Rajani                             #
-#   Disclaimer: Please handle private keys with care and securely,          #
-#   author is not responsible in any way for any damage caused              #
-#                                                                           #
-#############################################################################
+#########################################################################################
+#   Shell script which accepts p12 file and extracts following files                    #
+#   <p12filename>pub.pem - having just the public cert                                  #
+#   <p12filename>pubfc.pem - full chain of public cert                                  #
+#   <p12filename>pubx5c.pem - full chain of public cert in ready to use x5c format      #
+#   <p12filename>priv.key - unencrypted private RSA key                                 #
+#                                                                                       #
+#   Note: the above files are created in current directory                              #
+#                                                                                       #
+#   Version 1.0                                                                         #
+#   Developed by: Rajiv Rajani                                                          #
+#   Disclaimer: Please handle private keys with care and securely,                      #
+#   author is not responsible in any way for any damage caused                          #
+#                                                                                       #
+#########################################################################################
 
 #capture arg
 PKCSFL=$1
